060799338a5ee5b48edb4a6de5fb84ed44e42442
Lucas Brum <lucasmatheusrodriguesbrum@gmail.com>
2020-02-12 00:04:39 -0300

    Atualização de player de musica

--- player.py
+++ player.py
@@ -1,31 +1,31 @@
 from playsound import playsound
+from random import shuffle
 modo = 0
+playlist = ['musica 1.mp3', 'musica 2.mp3', 'musica 3.mp3']
 musica = 0
 print('Bem vindo ao midea player!!')
 print('Escolha um metodo do player: ')
-print('1 - tocar musica especifica: ')
-print('2 - tocar musica na sequência: ')
-print('3 - tocar musicas de forma aleatoria: ')
-modo = int(input('Digite um valor valido e inteiro de 1 a 3: '))
+print('1 - Twenty One Pilots: Stressd Out. ')
+print('2 - MC Leandrinho: Bonde do Tigrão. ')
+print('3 - Linkin Park: Numb Encore. ')
+print('0 - Tocar uma musica aleatoria.')
+
+modo = int(input('Digite um valor valido e inteiro de 0 a 3: '))
 if modo == 1:
-    print('Escolha uma musica: ')
-    print('1 - stresse out')
-    print('2 - bonde do tigrão')
-    print('3 - numb encore')
-    musica = int(input('Digite um valor valido e inteiro de 1 a 3: '))
-    if musica == 1:
-        playsound('musica.mp3')
-    if musica == 2:
-        playsound('musica.mp3')
-    if musica == 3:
-        playsound('musica.mp3')
-if modo == 2:
-    print('Sequência:')
-    print('1 - stresse out')
-    print('2 - bonde do tigrão')
-    print('3 - numb encore')
-    playsound('musica.mp3')
-    playsound('musica.mp3')
-    playsound('musica.mp3')
-if modo == 3:
-    playsound('musica.mp3')
+    print('Play Twenty one pilots: Stressd Out...')
+    playsound(playlist[0])
+else:
+    if modo == 2:
+        print('Play MC Leandrinho: Bonde do tigrão...')
+        playsound(playlist[1])
+    else:
+        if modo == 3:
+            print('Play Linkin Park: Numb Encore...')
+            playsound(playlist[2])
+        else:
+            if modo == 0:
+                print('Musica elatoria...')
+                shuffle(playlist)
+                playsound(playlist[0])
+            else:
+                print('\33[1:31mMODO INVALIDO!')
